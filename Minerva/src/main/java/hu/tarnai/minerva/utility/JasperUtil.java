67d8dcaccbe5429a0acc2dd3784fe47eedbd261a
tarnai.gabor <tarnai.gabor@MCEMER010.matrix2.meditcom.hu>
2018-06-04 16:35:20 +0200

    Jasper call

--- Minerva/src/main/java/hu/tarnai/minerva/utility/JasperUtil.java
+++ Minerva/src/main/java/hu/tarnai/minerva/utility/JasperUtil.java
@@ -1,7 +1,17 @@
 package hu.tarnai.minerva.utility;
 
 public class JasperUtil {
-
-	
+//	(JasperReport) JRLoader.loadObject(getReportStream(basicFileName + localeHu + EXTENTION))
+//	parameters.put("docRepDs", new CustomDataSource(labRes.toArray(new LabOrderRes[labRes.size()])));
+//	parameters.put("SUBREPORT_DIR", /* reportDirectory + "/" */REPORT_DIR);
+//	
+//	JasperReport report = getLocalizedJasper("LabOrderResult_la_viewer", session);
+//	
+//	byte[] pdfBlob;
+//	pdfBlob = JasperRunManager.runReportToPdf(report, parameters, new CustomDataSource(new Object[] { new Object() } ));
+//	OutputStream output;
+//	
+//	output = response.getOutputStream();
+//	output.write(pdfBlob);
 	
 }
